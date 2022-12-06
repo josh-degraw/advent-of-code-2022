7dfd5d0fdca716f0166ce368ee0d557fdca2a079
Josh DeGraw <josh-degraw@pluralsight.com>
2022-12-06 10:18:16 -0700

    Day 5, works for sample but not full input :/

--- day05/day05.fsx
+++ day05/day05.fsx
@@ -1,58 +1,64 @@
 open System
-open System.Text
 open System.Text.RegularExpressions
 open System.IO
 open System.Collections.Generic
 
-let inputText =
-  File.ReadAllText(Path.Combine(__SOURCE_DIRECTORY__, "./sample-input.txt"))
+let inputText () =
+  File.ReadAllText (Path.Combine (__SOURCE_DIRECTORY__, "./input.txt"))
 
-type Crate = { Id: int; Stack: Stack<string> }
+type Crate = Stack<string>
 
-type Instruction = { From: int; To: int; Count: int }
+type Instruction =
+  {
+    From: int
+    To: int
+    Count: int
+  }
 
+let printCrates (crates: Map<int, Crate>) =
+  for crate in crates do
+    printfn "%d:  %s" crate.Key (String.Join (" ", crate.Value))
+
+  printfn ""
 
 let parseLine (line: string) =
-  let itemRegex = new Regex("""(\[\w\] ?|   )""")
+  let itemRegex = new Regex ("""(\[\w\] ?|   )""")
 
-  [ let mutable i = 1
+  [
+    let mutable i = 1
 
-    for match' in itemRegex.Matches(line) do
-      match match'.Value.Trim() with
+    for match' in itemRegex.Matches (line) do
+      match match'.Value.Trim () with
       | "" -> i, None
       | value -> i, Some value
 
-      i <- i + 1 ]
+      i <- i + 1
+  ]
 
 let parseCrates (input: string) =
-  let crates = Dictionary<int, Crate>()
+  input.Split (Environment.NewLine)
+  |> Array.rev
+  |> Array.skip 1
+  |> Array.fold
+       (fun crates line ->
+         parseLine line
+         |> List.fold
+              (fun crates (id, contents) ->
+                crates
+                |> Map.change id (fun maybeExisting ->
+                  let crate = maybeExisting |> Option.defaultWith Crate
+                  contents |> Option.iter crate.Push
+                  Some crate))
+              crates)
+       Map.empty
 
-  let lines = input.Split(Environment.NewLine)
-
-  for line in input.Split(Environment.NewLine) |> Array.take (lines.Length - 1) do
-
-    let parsed = parseLine line
-
-    for id, contents in parsed do
-      let crate =
-        if crates.ContainsKey id then
-          crates.Item id
-        else
-          let newCrate = { Id = id; Stack = Stack() }
-          crates.Add(id, newCrate)
-          newCrate
-
-      match contents with
-      | None -> ()
-      | Some c -> crate.Stack.Push(c)
-
-  crates
 
 let parseInstructions (input: string) =
-  let reg = new Regex("""move (\d+) from (\d+) to (\d+)""")
+  let reg = new Regex ("""move (\d+) from (\d+) to (\d+)""")
 
-  [ for line in input.Split(Environment.NewLine) do
-      let match' = reg.Match(line)
+  [
+    for line in input.Split (Environment.NewLine) do
+      let match' = reg.Match (line)
 
       if not match'.Success then
         failwith "Invalid input"
@@ -60,17 +66,46 @@ let parseInstructions (input: string) =
       let groups = match'.Groups
       let count, from, to' = groups.[1], groups.[2], groups.[3]
 
-      { From = int from.Value
+      {
+        From = int from.Value
         To = int to'.Value
-        Count = int count.Value } ]
+        Count = int count.Value
+      }
+  ]
 
-let crates, instructions =
-  let split = inputText.Split(Environment.NewLine + Environment.NewLine)
+let parseSegments () =
+  let split = inputText().Split (Environment.NewLine + Environment.NewLine)
 
   match split with
   | [| crates; instructions |] -> parseCrates crates, parseInstructions instructions
   | _ -> failwith "Invalid input"
 
 
-//let stringify (this: Crate) = String.Join(";", this.Stack)
-//crates.Values |> Seq.map stringify |> Seq.toList
+let partOne =
+  let mutable i = 1
+  let crateMap, instructions = parseSegments ()
+
+  printCrates crateMap
+
+  for command in instructions do
+
+    let fromCrate = crateMap.Item command.From
+    let toCrate = crateMap.Item command.To
+
+    printfn $"Step {i}: move {command.Count} from {command.From} to {command.To}"
+
+    for _ in 1 .. command.Count do
+      let item = fromCrate.Pop ()
+      toCrate.Push item
+
+      printCrates crateMap
+      i <- i + 1
+
+  seq {
+    for crate in crateMap do
+      let hasVal, value = crate.Value.TryPeek ()
+
+      if hasVal then
+        value.Trim ('[', ']')
+  }
+  |> fun str -> String.Join ("", str)
